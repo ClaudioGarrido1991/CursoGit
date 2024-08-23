90dd90716415d36322c58c825e38c90dcdeeb8d5
ClaudioGarrido1991 <claudiogarridoarias@gmail.com>
2024-08-23 16:30:13 -0400

    Borra rama miguelAncapan

--- Clase09_matemáticas.java
+++ Clase09_matemáticas.java
@@ -2,7 +2,7 @@ public class Clase09_matemáticas { //clase propia
 
     public static void main(String[] args) {
 
-        
+        /* */
 
         double raiz = Math.sqrt(9); //raiz trabaja con double, por eso debe castearse para una salida INT
 

242901714c63ac1f3e26d7c3576f178e9c4e48cb
ClaudioGarrido1991 <claudiogarridoarias@gmail.com>
2024-08-23 13:00:18 -0400

    agregue nuevo archivo 4 y 5

--- nueva_carpeta/nuevo_archivo3.java
+++ nueva_carpeta/nuevo_archivo3.java
@@ -3,8 +3,14 @@ package nueva_carpeta;
 public class nuevo_archivo3 {
 
     public static void main(String[] args) {
+
+        int num1 = 1;
+        int num2 = 2;
+
+        System.out.println((num1+num2));
+        System.out.println(num1);
         
         System.out.println();
     }
-    //comentario
+    //comentario 2
 }

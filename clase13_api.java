da81ecf6f916c2e45644542d93205539ee3e6f2d
ClaudioGarrido1991 <claudiogarridoarias@gmail.com>
2024-08-23 01:02:21 -0400

    comentario

---
+++ clase13_api.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; //Directiva import, se usa para importar parquetes o clases.
+//import java.util.*; //Importa el paquete completo, esto utiliza más memoria.
+
+public class clase13_api {
+    /*NOTAS
+     * 
+     * Java.lang : Se considera el paquete Java por defecto o paquete principal.
+     */
+    
+    public static void main(String[] args) {
+
+        String nombre; //no hay problemas en utilizar la clase String porque esta incluida en Java.lang;
+        nombre = "Claudio";
+        System.out.println(nombre);
+
+        Scanner myScanner = new Scanner (System.in); //necesita importar java. util. Scanner;
+        int var = myScanner.nextInt(); //Para invocar un método debes agregar (), sino no funciona;
+        System.out.println(var);
+
+        myScanner.close();
+        
+    }
+}
